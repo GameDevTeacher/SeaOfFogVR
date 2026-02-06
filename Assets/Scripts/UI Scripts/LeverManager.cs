cb74b3ce527cc5ddce3aac04102e2c5a90667010
Marmine15 <martinandreas2006@gmail.com>
2026-02-06 10:16:24 +0100

    the lever now works

--- Assets/Scripts/UI Scripts/LeverManager.cs
+++ Assets/Scripts/UI Scripts/LeverManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UIElements;
 
 public class LeverManager : MonoBehaviour
 {
@@ -9,15 +10,24 @@ public class LeverManager : MonoBehaviour
     [Header("Lever")]
     [SerializeField] private HingeJoint lever;
     [SerializeField] private Rigidbody leverRigidbody;
-
+    [SerializeField] private Transform leverTransform;
+    
+    [Header("Limits")]
+    [SerializeField] private float maxLimit;
+    [SerializeField] private float minLimit;
+    
+    // Current issues: at the start, we get NaN. In the update, the value doesn't go below 50.
+    // changing it to the transform doesn't help either.
+    
     private void Start()
     {
-        // Mainly for testing
+        //todo: asign a starting value on start, starting value of 50,preferably then saved&pulled to a settings config
+        leverTransform.eulerAngles = new Vector3(0, leverTransform.eulerAngles.y, 0);
+        
         if (lever.useLimits)
         {
-            var value = Mathf.Round(Mathf.Clamp(lever.angle, lever.limits.min, lever.limits.max));
-            text.text = "Value: " + ScaleRegulator(value);
-            print(lever.angle);
+            var value = Mathf.Clamp(lever.angle, minLimit, maxLimit);
+            text.text = "Value: " + Mathf.Round(ScaleRegulator(value));
         }
     }
 
@@ -25,14 +35,17 @@ public class LeverManager : MonoBehaviour
     {
         if (leverRigidbody.angularVelocity != Vector3.zero)
         {
-            var value = Mathf.Round(Mathf.Clamp(lever.angle, lever.limits.min, lever.limits.max));
-            text.text = "Value: " + ScaleRegulator(value);
+            var value = Mathf.Clamp(lever.angle, minLimit, maxLimit);
+            print("The current value is: " + value);
+            print("When regulated, that value becomes: " + ScaleRegulator(value));
+            text.text = "Value: " + Mathf.Round(ScaleRegulator(value));
             print(Mathf.Round(lever.angle));
         }
     }
 
     private float ScaleRegulator(float value)
     {
+        print("value is" + value);
         return (value + 75) * 2 / 3;
     }
 }
