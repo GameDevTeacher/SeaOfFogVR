1ae15a1ac0af129ee598d99a6c964b13685275a7
Lacrimosa-GH <lacrimosa1457@gmail.com>
2026-02-06 14:31:25 +0100

    Build testable EchoInteraction and interactable cube prefab

--- Assets/Scripts/Storyteller/Listener/EchoInteractListener.cs
+++ Assets/Scripts/Storyteller/Listener/EchoInteractListener.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class EchoInteractListener : MonoBehaviour
 {
     [SerializeField] private string EchoFileName;
-    public string[] echoes; 
+    public string[] echoes;
+    private Material _materialColour;
     
     public void Start()
     {
         StoryEventsController.current.onEchoInteraction += PlayEcho;
         echoes = new []{"Test_echo_1",  "Test_echo_2", "Test_echo_3"};
-        
+        _materialColour = GetComponent<MeshRenderer>().material;
     }
 
     public void PlayEcho()
@@ -21,6 +22,8 @@ public class EchoInteractListener : MonoBehaviour
             if (echo.Equals(EchoFileName, StringComparison.OrdinalIgnoreCase))
             {
                 Debug.Log("Echo: " + echo);
+                _materialColour.color = Color.chartreuse;
+                
             }
         }
     }
