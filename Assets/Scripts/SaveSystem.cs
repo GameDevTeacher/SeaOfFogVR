240e4451db4a9ea4f8a4b2cbc526e141aeeeaee0
Galadan <arturas026@gmail.com>
2026-02-09 16:04:59 +0100

    SaveSystem Setup

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.IO;
+
+public class SaveSystem
+{
+    [System.Serializable]
+    public struct SaveData
+    {
+        //public PlayerSaveData PlayerData;
+        //boat save data
+        //world save data
+        
+    }
+}
