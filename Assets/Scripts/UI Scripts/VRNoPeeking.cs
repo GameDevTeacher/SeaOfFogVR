773bb1aef037a2e977d0790f5aeeb43a4f0860f0
Marmine15 <martinandreas2006@gmail.com>
2026-02-09 14:22:02 +0100

    set speeds
    you can now set speeds for fading in and fading out

--- Assets/Scripts/UI Scripts/VRNoPeeking.cs
+++ Assets/Scripts/UI Scripts/VRNoPeeking.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class VRNoPeeking : MonoBehaviour
 {
     [SerializeField] private LayerMask collisionLayer;
-    [SerializeField] private float fadeSpeed;
+    [SerializeField] private float fadeOutSpeed;
+    [SerializeField] private float fadeInSpeed;
     [SerializeField] private float sphereCheckSize = 0.15f;
 
     private Material _cameraFadeMat;
@@ -22,26 +23,37 @@ public class VRNoPeeking : MonoBehaviour
     {
         if (Physics.CheckSphere(transform.position, sphereCheckSize, collisionLayer, QueryTriggerInteraction.Ignore))
         {
-            CameraFade(1f);
+            CameraFadeOut(1f);
             _isCameraFadedOut = true;
         }
         else if (_mainMenuManager.shouldCameraFade && !_isCameraFadedOut)
         {
-            CameraFade(1f);
+            CameraFadeOut(1f);
         }
         else
         {
             if (!_isCameraFadedOut) return;
             
-            CameraFade(0f);
+            CameraFadeIn(0f);
         }
     }
 
-    public void CameraFade(float targetAlpha)
+    public void CameraFadeOut(float targetAlpha)
     {
         var alphaName = Shader.PropertyToID("_AlphaValue");
         var fadeValue = Mathf.MoveTowards(_cameraFadeMat.GetFloat(alphaName), targetAlpha, 
-            Time.deltaTime * fadeSpeed);
+            Time.deltaTime / fadeOutSpeed);
+        _cameraFadeMat.SetFloat(alphaName, fadeValue);
+        
+        if (fadeValue <= 0.01f)
+            _isCameraFadedOut = false;
+    }
+
+    public void CameraFadeIn(float targetAlpha)
+    {
+        var alphaName = Shader.PropertyToID("_AlphaValue");
+        var fadeValue = Mathf.MoveTowards(_cameraFadeMat.GetFloat(alphaName), targetAlpha, 
+            Time.deltaTime / fadeInSpeed);
         _cameraFadeMat.SetFloat(alphaName, fadeValue);
         
         if (fadeValue <= 0.01f)
