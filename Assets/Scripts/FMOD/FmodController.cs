a93fc331b630cc98eac7bde9a26664d753f78b29
Lacrimosa-GH <lacrimosa1457@gmail.com>
2026-02-10 11:07:25 +0100

    Link fmod to bank

---
+++ Assets/Scripts/FMOD/FmodController.cs
@@ -0,0 +1,19 @@
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+
+public class FmodController : MonoBehaviour
+{
+    [SerializeField] private EventInstance havtake;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
