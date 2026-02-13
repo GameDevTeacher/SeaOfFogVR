e5683c8f1d611eccd895709fa0e837f56835bf0d
Galadan <arturas026@gmail.com>
2026-02-13 13:26:38 +0100

    SpriteLightsFinished
    -sprite light prefab
    -sprite light manager prefab

---
+++ Assets/Scripts/SpriteLight.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpriteLight : MonoBehaviour
+{
+    private Material _material;
+    private bool _fadeOutTriggered = false;
+    public float fadeOutTime = 0.05f;
+    [SerializeField] private SphereCollider _collider;
+
+    private void Awake()
+    {
+        _material = GetComponent<Renderer>().material;
+        _material.color = new Color(_material.color.r,_material.color.g,_material.color.b,0);
+        _collider = GetComponent<SphereCollider>();
+        _collider.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_fadeOutTriggered) return;
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SpriteFadeOut();
+        }
+    }
+
+    public void SpriteFadeIn()
+    {
+        _collider.enabled = true;
+        StartCoroutine(FadeIn());
+    }
+
+    public void SpriteFadeOut()
+    {
+        _fadeOutTriggered  = true;
+        StartCoroutine(FadeOut());
+    }
+    
+    private IEnumerator FadeIn()
+    {
+        while (_material.color.a < 1)
+        {
+            if (_fadeOutTriggered)
+            {
+                yield break;
+            }
+            _material.color += new Color(0, 0, 0, 0.1f);
+            yield return new WaitForSeconds(fadeOutTime);
+        }
+    }
+
+    private IEnumerator FadeOut()
+    {
+        while (_material.color.a > 0.1f)
+        {
+            _material.color -= new Color(0, 0, 0, 0.1f);
+            yield return new WaitForSeconds(fadeOutTime);
+        }
+        gameObject.SetActive(false);
+        SpriteLightManager.instance.UpdateSpriteScript();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _collider.radius/2);
+    }
+}
