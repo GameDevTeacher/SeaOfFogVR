900ce28a7038d7b5334b3b804aeb6eefe29e8d6e
Lacrimosa-GH <lacrimosa1457@gmail.com>
2026-02-13 14:38:22 +0100

    New echo interact script

--- Assets/Scripts/Storyteller/Listener/EchoInteractListener.cs
+++ Assets/Scripts/Storyteller/Listener/EchoInteractListener.cs
@@ -4,35 +4,5 @@ using UnityEngine;
 
 public class EchoInteractListener : MonoBehaviour
 {
-    [SerializeField] private string EchoFileName;
-    public string[] echoes;
-    private Material _materialColour;
-    
-    public void Start()
-    {
-        /*This enables testing using the custom event system,
-          can be triggered in XR interactable
-          Select event instead*/
-        StoryEventsController.current.onEchoInteraction += PlayEcho;
-        
-        echoes = new []{"Test_echo_1",  "Test_echo_2", "Test_echo_3"};
-        _materialColour = GetComponent<MeshRenderer>().material;
-    }
-
-    
-    public void PlayEcho()
-    {
-        foreach (var echo in echoes)
-        {
-            if (echo.Equals(EchoFileName, StringComparison.OrdinalIgnoreCase))
-            {
-                //Replace with audio later
-                Debug.Log("Echo: " + echo);
-                _materialColour.color = Color.chartreuse;
-                
-            }
-        }
-    }
-
-    
+    public void PlayEcho(string filepath) => FmodController.current.UpdateEchoTrigger(filepath); 
 }
