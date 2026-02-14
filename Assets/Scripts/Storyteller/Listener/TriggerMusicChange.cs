dbc8b120e75fc481c6cbe8b6b98a48991e98250d
Lacrimosa-GH <lacrimosa1457@gmail.com>
2026-02-14 12:10:15 +0100

    updated fmod prefabs ad finished lighthouse isle placements

--- Assets/Scripts/Storyteller/Listener/TriggerMusicChange.cs
+++ Assets/Scripts/Storyteller/Listener/TriggerMusicChange.cs
@@ -5,29 +5,13 @@ public class TriggerMusicChange : MonoBehaviour
 
 
     [SerializeField] private int AmbienceSectionChange;
-    void Start()
-    {
-        StoryEventsController.current.onTriggerEntered += TriggerEntered;
-        StoryEventsController.current.onTriggerExited += TriggerExited;
-        
-    }
 
-    private void TriggerEntered(int id)
+    private void OnTriggerEnter(Collider other) 
     {   
-        if (id == AmbienceSectionChange)
-        {
-            GetComponent<MeshRenderer>().material.color = Color.green;
-            Debug.Log("Entered Trigger " + AmbienceSectionChange);
+        
+        Debug.Log(other + "Entered Trigger " + AmbienceSectionChange);
 
-            FmodController.current.UpdateSection(AmbienceSectionChange);
-        }
-    }
-
-    private void TriggerExited(int id)
-    { if (id == AmbienceSectionChange)
-        {
-            GetComponent<MeshRenderer>().material.color = Color.dodgerBlue;
-            Debug.Log("Exited Trigger");
-        }
+        FmodController.current.UpdateSection(AmbienceSectionChange);
+        
     }
 }
