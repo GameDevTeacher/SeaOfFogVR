c895b619edd821df93befbcae7c9d4f21d7babe6
Frederik Ingolv Friquin <frederik.friquin@gmail.com>
2026-02-16 15:05:09 +0100

    Fog now moves properly

--- Assets/Scripts/Storyteller/FogController.cs
+++ Assets/Scripts/Storyteller/FogController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,13 +15,30 @@ public class FogController : MonoBehaviour
    
    [SerializeField] private List<GameObject> _fogList;
    [SerializeField] private int _fogIndex;
-   
-   
+   [SerializeField] private int _lastFog;
+   [SerializeField] private bool _startWait;
+
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.gameObject.tag == "Player" || other.gameObject.layer == LayerMask.NameToLayer("Boat"))
+      _startWait = true;
+   }
+
    public void NextFog()
    {
-      _fogList[_fogIndex].SetActive(false);
+      _lastFog = _fogIndex;
       if (_fogIndex == _fogList.Count - 1) _fogIndex = 0;
       else _fogIndex++;
-      _fogList[_fogIndex].SetActive(true);
+      StartCoroutine(_nextFog());
+   }
+   
+   
+   private IEnumerator _nextFog()
+   {
+         _fogList[_fogIndex].SetActive(true);
+         yield return new WaitForSeconds(1f);
+         _fogList[_lastFog].SetActive(false);
+         _startWait = false;
    }
 }
