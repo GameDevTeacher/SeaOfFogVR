ea888e651ff2b0f72aea2f9caaaf0c170c662a67
Frederik Ingolv Friquin <frederik.friquin@gmail.com>
2026-02-17 17:32:11 +0100

    Fog tweaks

--- Assets/Scripts/Storyteller/FogController.cs
+++ Assets/Scripts/Storyteller/FogController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class FogController : MonoBehaviour
 {
@@ -10,18 +11,23 @@ public class FogController : MonoBehaviour
    private void Awake()
    {
       if (instance == null)  instance = this;
-      else Destroy(gameObject);
+      else if (instance != this) Destroy(gameObject);
+      foreach (GameObject fog in _fogList)
+      {
+         if (fog != _fogList[_fogIndex]) fog.GetComponent<VisualEffect>().Stop();
+      }
    }
    
    [SerializeField] private List<GameObject> _fogList;
    [SerializeField] private int _fogIndex;
    [SerializeField] private int _lastFog;
    [SerializeField] private bool _startWait;
-
+   
+   
 
    private void OnTriggerEnter(Collider other)
    {
-      if (other.gameObject.tag == "Player" || other.gameObject.layer == LayerMask.NameToLayer("Boat"))
+      if ((other.gameObject.tag == "Player" || other.gameObject.layer == LayerMask.NameToLayer("Boat"))&& !_startWait)
       _startWait = true;
    }
 
@@ -36,9 +42,9 @@ public class FogController : MonoBehaviour
    
    private IEnumerator _nextFog()
    {
-         _fogList[_fogIndex].SetActive(true);
+         _fogList[_fogIndex].GetComponent<VisualEffect>().Play();
          yield return new WaitForSeconds(1f);
-         _fogList[_lastFog].SetActive(false);
+         _fogList[_lastFog].GetComponent<VisualEffect>().Stop();
          _startWait = false;
    }
 }
