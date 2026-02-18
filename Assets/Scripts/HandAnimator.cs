8e9bd2d7357ab46d24bb9c0d69f29ad8d040c59a
Lacrimosa-GH <lacrimosa1457@gmail.com>
2026-02-18 11:01:46 +0100

    hands implemented

---
+++ Assets/Scripts/HandAnimator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class HandAnimator : MonoBehaviour
+{
+
+    [SerializeField] private MeshRenderer m_MeshRenderer;
+    [SerializeField] private MeshFilter m_MeshFilter;
+    
+    private void Start()
+    {
+        
+    }
+}
